7717327fa9d61a0e30ff3f8d1db75927c5103f01
Ilya Verenich <ilyavere@gmail.com>
2018-04-12 21:14:28 +1000

    add 7 more logs (from my flow analysis paper)

--- experiments/dataset_confs.py
+++ experiments/dataset_confs.py
@@ -122,3 +122,142 @@ static_cat_cols[dataset] = ['DiagnosticArtAstrup', 'DiagnosticBlood', 'Diagnosti
        'SIRSCritTemperature', 'SIRSCriteria2OrMore']
 dynamic_num_cols[dataset] = ['CRP', 'LacticAcid', 'Leucocytes', "duration", "month", "weekday", "hour"]
 static_num_cols[dataset] = ['Age']
+
+
+
+#### BPI2012A settings ####
+dataset = "bpic2012a"
+
+filename[dataset] = "labeled_logs_csv_processed/BPI2012A.csv"
+
+case_id_col[dataset] = "case_id"
+activity_col[dataset] = "activity_name"
+timestamp_col[dataset] = "time"
+label_col[dataset] = "remtime"
+pos_label[dataset] = "regular"
+neg_label[dataset] = "deviant"
+
+# features for classifier
+dynamic_cat_cols[dataset] = ['activity_name', 'Resource']
+static_cat_cols[dataset] = []
+dynamic_num_cols[dataset] = ['open_cases','elapsed']
+static_num_cols[dataset] = ['AMOUNT_REQ']
+
+
+
+#### BPI2012O settings ####
+dataset = "bpic2012o"
+
+filename[dataset] = "labeled_logs_csv_processed/BPI2012O.csv"
+
+case_id_col[dataset] = "case_id"
+activity_col[dataset] = "activity_name"
+timestamp_col[dataset] = "time"
+label_col[dataset] = "remtime"
+pos_label[dataset] = "regular"
+neg_label[dataset] = "deviant"
+
+# features for classifier
+dynamic_cat_cols[dataset] = ['activity_name', 'Resource']
+static_cat_cols[dataset] = []
+dynamic_num_cols[dataset] = ['open_cases','elapsed']
+static_num_cols[dataset] = ['AMOUNT_REQ']
+
+
+
+#### BPI2012W settings ####
+dataset = "bpic2012w"
+
+filename[dataset] = "labeled_logs_csv_processed/BPI2012W.csv"
+
+case_id_col[dataset] = "case_id"
+activity_col[dataset] = "activity_name"
+timestamp_col[dataset] = "time"
+label_col[dataset] = "remtime"
+pos_label[dataset] = "regular"
+neg_label[dataset] = "deviant"
+
+# features for classifier
+dynamic_cat_cols[dataset] = ['activity_name', 'Resource']
+static_cat_cols[dataset] = []
+dynamic_num_cols[dataset] = ['open_cases','elapsed','proctime']
+static_num_cols[dataset] = ['AMOUNT_REQ']
+
+
+
+#### Credit requirements settings ####
+dataset = "credit"
+
+filename[dataset] = "labeled_logs_csv_processed/CreditRequirement.csv"
+
+case_id_col[dataset] = "case_id"
+activity_col[dataset] = "Activity"
+timestamp_col[dataset] = "Complete Timestamp"
+label_col[dataset] = "remtime"
+pos_label[dataset] = "regular"
+neg_label[dataset] = "deviant"
+
+# features for classifier
+dynamic_cat_cols[dataset] = ['Activity','weekday','hour']
+static_cat_cols[dataset] = []
+dynamic_num_cols[dataset] = ['open_cases','timesincecasestart','timesincemidnight','activity_duration']
+static_num_cols[dataset] = []
+
+
+
+#### helpdesk settings ####
+dataset = "helpdesk"
+
+filename[dataset] = "labeled_logs_csv_processed/helpdesk.csv"
+
+case_id_col[dataset] = "case_id"
+activity_col[dataset] = "activity_name"
+timestamp_col[dataset] = "time"
+label_col[dataset] = "remtime"
+pos_label[dataset] = "regular"
+neg_label[dataset] = "deviant"
+
+# features for classifier
+dynamic_cat_cols[dataset] = ['activity_name','Resource']
+static_cat_cols[dataset] = ["customer", "product",   "responsible_section",  "seriousness",  "service_level",   "service_type",  "support_section"]
+dynamic_num_cols[dataset] = ['open_cases','elapsed']
+static_num_cols[dataset] = []
+
+
+#### hospital billing settings ####
+dataset = "hospital"
+
+filename[dataset] = "labeled_logs_csv_processed/hospital_billing_977.csv"
+
+case_id_col[dataset] = "case_id"
+activity_col[dataset] = "Activity"
+timestamp_col[dataset] = "Complete Timestamp"
+label_col[dataset] = "remtime"
+pos_label[dataset] = "regular"
+neg_label[dataset] = "deviant"
+
+# features for classifier
+dynamic_cat_cols[dataset] = ["Activity", "Resource","actOrange","actRed", "blocked", "caseType", "diagnosis", "flagC","flagD", "msgCode", "msgType", "state", "version"]
+static_cat_cols[dataset] = ["speciality"]
+dynamic_num_cols[dataset] = ["msgCount",   "timesincelastevent",    "timesincecasestart",    "event_nr",    "weekday",    "hour",    "open_cases"]
+static_num_cols[dataset] = []
+
+
+
+#### minit invoice settings ####
+dataset = "minit"
+
+filename[dataset] = "labeled_logs_csv_processed/minit_invoice_10.csv"
+
+case_id_col[dataset] = "Case ID"
+activity_col[dataset] = "Activity"
+timestamp_col[dataset] = "Complete Timestamp"
+label_col[dataset] = "remtime"
+pos_label[dataset] = "regular"
+neg_label[dataset] = "deviant"
+
+# features for classifier
+dynamic_cat_cols[dataset] = ["Activity", "Resource",  "ActivityFinalAction",  "EventType",  "weekday",  "hour"]
+static_cat_cols[dataset] = ["CostCenter.Code",  "Supplier.City",    "Supplier.Name",    "Supplier.State"]
+dynamic_num_cols[dataset] = ["open_cases",    "timesincelastevent",    "timesincecasestart",    "event_nr"]
+static_num_cols[dataset] = ["InvoiceTotalAmountWithoutVAT"]
